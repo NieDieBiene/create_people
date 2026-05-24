352a08b00e6317354b89aa832364c1cd26c8fd29
NieDieBiene <diebienepodp@gmail.com>
2026-05-24 19:18:47 +0300

    Update create_people.py

--- create_people.py
+++ create_people.py
@@ -1,57 +1,61 @@
 import file_operations
-from faker import Faker
 import random
 import os
 import shutil
+from faker import Faker
 
 
 def create_empty_folder(folder_name="people"):
-	
-	if os.path.exists(folder_name):
-	    shutil.rmtree(folder_name)
+    os.makedirs(folder_name, exist_ok=True)
+    return folder_name
+    
 
-	os.makedirs(folder_name)
-	return folder_name
+def create_skills():	
+    letters = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+    }
 
-def create_skills():
-	letters = {
-	    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-	    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-	    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-	    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-	    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-	    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-	    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-	    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-	    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-	    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-	    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-	    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-	    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-	    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-	    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-	    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-	    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-	    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-	    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-	    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-	    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-	    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-	    ' ': ' '
-	}
+    skills_list = [
+        "Стремительный прыжок",
+        "Электрический выстрел",
+        "Ледяной удар",
+        "Стремительный удар",
+        "Кислотный взгляд",
+        "Тайный побег",
+        "Ледяной выстрел",
+        "Огненный заряд"
+    ]
 
-	skills_list = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
-	"Стремительный удар", "Кислотный взгляд", "Тайный побег",
-	"Ледяной выстрел", "Огненный заряд",]
+    skills_list_runes = []
 
-	skills_list_runes = []
-
-	for skill in skills_list:
-		skill_rune = skill
-		for key, value in letters.items():
-			skill_rune = skill_rune.replace(key, value)
-		skills_list_runes.append(skill_rune)
-	return skills_list_runes
+    for skill in skills_list:
+        skill_rune = skill
+        for key, value in letters.items():
+            skill_rune = skill_rune.replace(key, value)
+        skills_list_runes.append(skill_rune)
+    return skills_list_runes
 		
 
 def create_people(folder_name, skills_list_runes):
@@ -77,13 +81,15 @@ def create_people(folder_name, skills_list_runes):
 		}
 
 		file_name = folder_name + "/homo{number}.svg"
-		file_name = file_name.format(number = str(number))
+		file_name = file_name.format(number=str(number))
 		file_operations.render_template("charsheet.svg", file_name, context)
+
 
 def main():
 	folder = create_empty_folder()
 	runes = create_skills()
 	create_people(folder, runes)
+
 
 if __name__ == '__main__':
 	main()
