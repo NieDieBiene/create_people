8bc90743b6b64f9f8854ae3f97c95c41971f6d6f
NieDieBiene <diebienepodp@gmail.com>
2026-05-29 12:06:11 +0300

    Update create_people.py

--- create_people.py
+++ create_people.py
@@ -5,35 +5,37 @@ from faker import Faker
 
 
 def create_empty_folder(folder_name="people"):
+
     os.makedirs(folder_name, exist_ok=True)
     return folder_name
-    
 
-def create_skills():	
+
+def create_skills():
+
     letters = {
-    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-    ' ': ' '
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
     }
 
     skills_list = [
@@ -55,40 +57,40 @@ def create_skills():
             skill_rune = skill_rune.replace(key, value)
         skills_list_runes.append(skill_rune)
     return skills_list_runes
-		
+
 
 def create_people(folder_name, skills_list_runes):
-	for number in range(10):
+    for number in range(10):
 
-		skills = random.sample(skills_list_runes, 3)
+        skills = random.sample(skills_list_runes, 3)
 
-		fake = Faker("ru_RU")
+        fake = Faker("ru_RU")
 
-		context = {
-		  "first_name": fake.first_name_male(),
-		  "last_name": fake.last_name_male(),
-		  "job": fake.job(),
-		  "town": fake.city(),
-		  "strength": random.randint(3, 18),
-		  "agility": random.randint(3, 18),
-		  "endurance": random.randint(3, 18),
-		  "intelligence": random.randint(3, 18),
-		  "luck": random.randint(3, 18),
-		  "skill_1": skills[0],
-		  "skill_2": skills[1],
-		  "skill_3": skills[2]
-		}
+        context = {
+            "first_name": fake.first_name_male(),
+            "last_name": fake.last_name_male(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": skills[0],
+            "skill_2": skills[1],
+            "skill_3": skills[2]
+        }
 
-		file_name = folder_name + "/homo{number}.svg"
-		file_name = file_name.format(number=str(number))
-		file_operations.render_template("charsheet.svg", file_name, context)
+        file_name = folder_name + "/homo{number}.svg"
+        file_name = file_name.format(number=str(number))
+        file_operations.render_template("charsheet.svg", file_name, context)
 
 
 def main():
-	folder = create_empty_folder()
-	runes = create_skills()
-	create_people(folder, runes)
+    folder = create_empty_folder()
+    runes = create_skills()
+    create_people(folder, runes)
 
 
 if __name__ == '__main__':
-	main()
+    main()
